c2ecb8b8b36e0b086251349c4eb45052f3f157d9
Guye Raphael, I233 <raphael.guye@post.ch>
2021-05-31 08:40:33 +0200

    Configure ViewController in code

--- CleanTodoList/ViewController.swift
+++ CleanTodoList/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var helloLabel: UILabel!
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        createComponents()
+        
+        addSubViews()
+        
+        applyLayoutConstraint()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func createComponents() {
+        helloLabel = UILabel()
+        helloLabel.translatesAutoresizingMaskIntoConstraints = false
+        helloLabel.textAlignment = .right
+        helloLabel.text = "Hello world !"
+    }
+    
+    private func addSubViews() {
+        view.addSubviewIfNotNil(helloLabel)
+    }
+    
+    private func applyLayoutConstraint() {
+        NSLayoutConstraint.activate([
+            helloLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            helloLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+}
 
-
+extension UIView {
+    func addSubviewIfNotNil(_ view: UIView?) {
+        if (view != nil) {
+            addSubview(view!)
+        }
+    }
 }
 
