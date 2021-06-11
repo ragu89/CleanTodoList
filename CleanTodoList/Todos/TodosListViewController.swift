2bdb88bca2212ec28eea30a9a862eeba0ea6f016
Guye Raphael, I233 <raphael.guye@post.ch>
2021-06-11 13:10:25 +0200

    Working on the TableViewDataSource

--- CleanTodoList/Todos/TodosListViewController.swift
+++ CleanTodoList/Todos/TodosListViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class TodosListViewController : UITableViewController {
     
+    var todos: [String]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Todo list"
+        
+        todos = ["todo 1", "todo 2", "todo 3"]
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+}
+
+extension TodosListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        todos?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = todos![indexPath.row]
+        return cell
     }
     
 }
