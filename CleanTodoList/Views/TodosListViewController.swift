b91af907fbcd9286f3171926ceecf03e7b931350
Guye Raphael, I233 <raphael.guye@post.ch>
2021-06-13 17:16:28 +0200

    wip

--- CleanTodoList/Views/TodosListViewController.swift
+++ CleanTodoList/Views/TodosListViewController.swift
@@ -26,8 +26,7 @@ class TodosListViewController : UITableViewController {
     }
     
     required init?(coder: NSCoder) {
-        self.viewModel = TodosListViewModel(todoService: FakeTodoService())
-        super.init(coder: coder)
+        fatalError("You must create this view controller with a ViewModel")
     }
     
     deinit {
